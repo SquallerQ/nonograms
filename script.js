731f2128701abcca195d0c2a90bc5aeb946d5f10
zSqualler <john199502031@gmail.com>
2025-01-26 14:15:06 +0100

    feat: add logic for creating cells on the left panel

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const matrix = [
   [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -27,7 +27,7 @@ function displayMatrix () {
   for (let i = 0; i < matrix.length; i++) {
     const row = document.createElement("div");
     row.classList.add('matrix__row')
-    console.log(row);
+    // console.log(row);
     
     matrixContainer.append(row);
     for (let j = 0; j < matrix[i].length; j++) {
@@ -57,13 +57,42 @@ function displayLeftPanel () {
   const leftPanelContainer = document.createElement('div')
   leftPanelContainer.classList.add("left-panel__container");
   body.append(leftPanelContainer);
-  console.log(leftPanelContainer);
   
-
   for (let i = 0; i < matrix.length; i++) {
-    const leftPanelCell = document.createElement('div');
-    leftPanelCell.classList.add('left-panel__cell');
-    leftPanelContainer.append(leftPanelCell);
+
+    const leftPanelRow = document.createElement("div");
+    leftPanelRow.classList.add("left-panel__row");
+    leftPanelContainer.append(leftPanelRow);
+    leftPanelRow.dataset.rowId = i;
+
+    const matrixRow = matrix[i];
+    let numberOfBlackCells = 0;
+    for (let j = 0; j < matrixRow.length; j++) {
+
+      if (matrixRow[j] > 0 && matrixRow[j + 1] > 0) {
+        numberOfBlackCells = numberOfBlackCells + 1;
+      } else if (matrixRow[j] > 0 && matrixRow[j + 1] === 0) {
+        numberOfBlackCells = numberOfBlackCells + 1;
+
+        const leftPanelCell = document.createElement("div");
+        leftPanelCell.classList.add("left-panel__cell");
+        leftPanelCell.innerText = numberOfBlackCells;
+        
+        const leftPanelRow = document.querySelector(`[data-row-id="${i}"]`);
+        leftPanelRow.append(leftPanelCell);
+
+        numberOfBlackCells = 0;       
+      }
+    }
+    if (!matrixRow.includes(1)) {
+      console.log(true);
+      const leftPanelCell = document.createElement("div");
+      leftPanelCell.classList.add("left-panel__cell");
+      leftPanelCell.innerText = numberOfBlackCells;
+      const leftPanelRow = document.querySelector(`[data-row-id="${i}"]`);
+      leftPanelRow.append(leftPanelCell);
+    }
+
   }
 
   // matrixContainer;
